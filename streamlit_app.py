1eadb089149830dc590cd34ac0ecd8734e6c14f7
MANGAINV <123408589+MANGAINV@users.noreply.github.com>
2023-01-24 09:09:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,5 @@ streamlit.header('🍒 🍇Built my own fruit smoothie🍓 🍑')
 
 import pandas
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
