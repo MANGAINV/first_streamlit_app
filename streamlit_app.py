af418bba0ec38734db132f057194bed7d61dacec
MANGAINV <123408589+MANGAINV@users.noreply.github.com>
2023-01-24 08:28:43 +0530

    Update streamlit_app.py
    Emoticons added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('BREAKFAST MENU')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🐔Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🍹Kale, Spinach & Rocket smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
 
