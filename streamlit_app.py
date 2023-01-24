77e6f162c819075c867f5ad9e28e77095f91c51c
MANGAINV <123408589+MANGAINV@users.noreply.github.com>
2023-01-24 08:11:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('BREAKFAST MENU')
+
+strealit.paragraph('Omega 3 & Blueberry Oatmeal')
+strealit.paragraph('Kale, Spinach & Rocket smoothie')
+strealit.paragraph('Hard-Boiled Free-Range Egg')
+
